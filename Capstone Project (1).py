7d277294465479211d9e8bf4dd89a64005450686
AbdullahAldossari <81445796+AbdullahAldossari@users.noreply.github.com>
2021-03-31 21:32:20 -0400

    Add files via upload

---
+++ Capstone Project (1).py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This Notebook is for the Capstone Project
+
+# IBM Data Science Professional Certificate
+
+# Author: Abdullah Aldossari 
+
+# In[1]:
+
+
+import pandas as pd 
+import numpy as np 
+print('Hello Capstone Project')
+
